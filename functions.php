2fc78b4757cfad2d8baf77bb0535129d6f795701
morganmdx <147529847+morganmdx@users.noreply.github.com>
2024-09-22 17:57:41 +0400

    Added menu support

--- functions.php
+++ functions.php
@@ -6,9 +6,18 @@ add_theme_support( 'custom-logo' ); // Allows adding a custom logo to the site.
 add_theme_support( 'html5', array( 'search-form', 'comment-form', 'gallery', 'caption' ) ); // Support for HTML5 markup.
 
 
+
 add_action( 'after_setup_theme', 'sheerwater_gardens_setup' );
 
 function sheerwater_gardens_setup() {
 	add_theme_support( 'wp-block-styles' );
+
+    // This theme uses wp_nav_menu() in two locations.
+		register_nav_menus(
+			array(
+				'primary'   => __( 'Top primary menu', 'twentyfourteen' ),
+				'secondary' => __( 'Secondary menu in left sidebar', 'twentyfourteen' ),
+			)
+		);
 }
 
