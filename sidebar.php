87cfa69a5c7464c23dec1f586e9355ca5bbf4ecc
morganmdx <147529847+morganmdx@users.noreply.github.com>
2024-09-22 18:54:38 +0400

    Remove site tagline from sidebar php

--- sidebar.php
+++ sidebar.php
@@ -7,12 +7,7 @@
  */
 ?>
 <div id="secondary">
-	<?php
-		$description = get_bloginfo( 'description', 'display' );
-	if ( ! empty( $description ) ) :
-		?>
-	<h2 class="site-description"><?php echo esc_html( $description ); ?></h2>
-	<?php endif; ?>
+	
 
 	<?php if ( has_nav_menu( 'secondary' ) ) : ?>
 	<nav class="navigation site-navigation secondary-navigation">
