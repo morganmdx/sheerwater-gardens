21d00a1169bdcc726a892245985aac135e71910c
morganmdx <147529847+morganmdx@users.noreply.github.com>
2024-09-28 03:57:31 +0400

    Fixes to functions and header php for browser title tag

--- functions.php
+++ functions.php
@@ -1,6 +1,6 @@
 <?php
 
-add_theme_support( 'title-tag' ); // Adds dynamic <title> tag support.
+add_theme_support('title-tag'); // Adds dynamic <title> tag support.
 add_theme_support( 'post-thumbnails' ); // Enables featured image support for posts.
 add_theme_support( 'custom-logo' ); // Allows adding a custom logo to the site.
 add_theme_support( 'html5', array( 'search-form', 'comment-form', 'gallery', 'caption' ) ); // Support for HTML5 markup.
--- header.php
+++ header.php
@@ -1,13 +1,3 @@
-<?php
-/**
- * The Header for our theme
- *
- * Displays all of the <head> section and everything up till <div id="main">
- *
- * @package WordPress
- * @subpackage sheerwater_gardens
- */
-?>
 <!DOCTYPE html>
 <!--[if IE 7]>
 <html class="ie ie7" <?php language_attributes(); ?>>
@@ -19,69 +9,62 @@
 <html <?php language_attributes(); ?>>
 <!--<![endif]-->
 <head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title><?php wp_title( '|', true, 'right' ); ?></title>
-	<link rel="profile" href="https://gmpg.org/xfn/11">
-	<link rel="pingback" href="<?php echo esc_url( get_bloginfo( 'pingback_url' ) ); ?>">
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="profile" href="https://gmpg.org/xfn/11">
+    <link rel="pingback" href="<?php echo esc_url( get_bloginfo( 'pingback_url' ) ); ?>">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=DM+Serif+Display:ital@0;1&family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap" rel="stylesheet">
-	<!--[if lt IE 9]>
-	<script src="<?php echo esc_url( get_template_directory_uri() ); ?>/js/html5.js?ver=3.7.0"></script>
-	<![endif]-->
-	<?php wp_head(); ?>
+    <link href="https://fonts.googleapis.com/css2?family=DM+Serif+Display:ital@0;1&family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900&display=swap" rel="stylesheet">
+    <!--[if lt IE 9]>
+    <script src="<?php echo esc_url( get_template_directory_uri() ); ?>/js/html5.js?ver=3.7.0"></script>
+    <![endif]-->
+    <?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
 <?php wp_body_open(); ?>
 <div id="page" class="hfeed site">
-	<?php if ( get_header_image() ) : ?>
-	<div id="site-header">
-		<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
-			abc
-		</a>
-	</div>
-	<?php endif; ?>
+    <?php if ( get_header_image() ) : ?>
+    <div id="site-header">
+        <a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
+            abc
+        </a>
+    </div>
+    <?php endif; ?>
 
-	<header id="masthead" class="site-header">
-		<div class="header-main">
-			<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+    <header id="masthead" class="site-header">
+        <div class="header-main">
+            <h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
 
-			<div class="search-toggle">
-				<a href="#search-container" class="screen-reader-text" aria-expanded="false" aria-controls="search-container">
-					<?php
-					/* translators: Hidden accessibility text. */
-					_e( 'Search', 'sheerwater_gardens' );
-					?>
-				</a>
-			</div>
+            <div class="search-toggle">
+                <a href="#search-container" class="screen-reader-text" aria-expanded="false" aria-controls="search-container">
+                    <?php _e( 'Search', 'sheerwater_gardens' ); ?>
+                </a>
+            </div>
 
-			<nav id="primary-navigation" class="site-navigation primary-navigation">
-				<button class="menu-toggle"><?php _e( 'Primary Menu', 'sheerwater_gardens' ); ?></button>
-				<a class="screen-reader-text skip-link" href="#content">
-					<?php
-					/* translators: Hidden accessibility text. */
-					_e( 'Skip to content', 'sheerwater_gardens' );
-					?>
-				</a>
-				<?php
-				wp_nav_menu(
-					array(
-						'theme_location' => 'primary',
-						'menu_class'     => 'nav-menu',
-						'menu_id'        => 'primary-menu',
-					)
-				);
-				?>
-			</nav>
-		</div>
+            <nav id="primary-navigation" class="site-navigation primary-navigation">
+                <button class="menu-toggle"><?php _e( 'Primary Menu', 'sheerwater_gardens' ); ?></button>
+                <a class="screen-reader-text skip-link" href="#content">
+                    <?php _e( 'Skip to content', 'sheerwater_gardens' ); ?>
+                </a>
+                <?php
+                wp_nav_menu(
+                    array(
+                        'theme_location' => 'primary',
+                        'menu_class'     => 'nav-menu',
+                        'menu_id'        => 'primary-menu',
+                    )
+                );
+                ?>
+            </nav>
+        </div>
 
-		<div id="search-container" class="search-box-wrapper hide">
-			<div class="search-box">
-				<?php get_search_form(); ?>
-			</div>
-		</div>
-	</header><!-- #masthead -->
+        <div id="search-container" class="search-box-wrapper hide">
+            <div class="search-box">
+                <?php get_search_form(); ?>
+            </div>
+        </div>
+    </header><!-- #masthead -->
 
-	<div id="main" class="site-main">
+    <div id="main" class="site-main">
