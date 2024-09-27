3c1dac2c14ac4118408b564af5d0720a019f7b4b
morganmdx <147529847+morganmdx@users.noreply.github.com>
2024-09-28 03:59:18 +0400

    Add bootstrap CSS and JS

--- functions.php
+++ functions.php
@@ -92,6 +92,9 @@ wp_localize_script(
     )
 );
 wp_enqueue_style( 'main_css', get_stylesheet_uri() );
+wp_enqueue_style( 'bootstrap-css', 'https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css' );
+wp_enqueue_script( 'bootstrap-js', 'https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js', array('jquery'), null, true );
+
 
 }
 add_action( 'wp_enqueue_scripts', 'sheerwater_gardens_scripts' );
